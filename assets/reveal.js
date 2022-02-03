d7b9b35a4f620e2a40da5e2613b0c1c1588ba3e5
Xavier <nicodemexavier@gmail.com>
2022-02-03 19:17:31 +0100

    changes delay scroll reveal

--- assets/reveal.js
+++ assets/reveal.js
@@ -7,9 +7,9 @@ ScrollReveal().reveal('.entete-3', { delay: 500, reset: true });
 ScrollReveal().reveal('.name', { delay: 700, reset: true });
 
 ScrollReveal().reveal('.projets', { reset: true});
-ScrollReveal().reveal('.projets-1', { delay:100, reset: true});
-ScrollReveal().reveal('.projets-2', { delay:200, reset: true});
-ScrollReveal().reveal('.projets-3', { delay:300, reset: true});
+ScrollReveal().reveal('.projets-1', { delay:100});
+ScrollReveal().reveal('.projets-2', { delay:200});
+ScrollReveal().reveal('.projets-3', { delay:300});
 
 ScrollReveal().reveal('.contact-1', { delay:100, reset: true});
 ScrollReveal().reveal('.contact-2', { delay:200, reset: true});
@@ -19,10 +19,10 @@ ScrollReveal().reveal('.contact-5', { delay:500, reset: true});
 
 
 ScrollReveal().reveal('.projet-page', { reset: true});
-ScrollReveal().reveal('.show-2', { delay:200});
+
 ScrollReveal().reveal('.show-3', { delay:300});
 ScrollReveal().reveal('.show-4', { delay:400});
 ScrollReveal().reveal('.show-5', { delay:500});
 ScrollReveal().reveal('.show-6', { delay:600});
-ScrollReveal().reveal('.show-7', { delay:700});
-ScrollReveal().reveal('.show-8', { delay:800});
+ScrollReveal().reveal('.show-7', { delay:600});
+ScrollReveal().reveal('.show-8', { delay:600});
